83e5929535a94c7898dc29631330666c53d53d18
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2020-09-12 09:05:56 -0400

    Add code in module 2

---
+++ DEV204.1/Mod2_Lab1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Mod2_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create an if decision block
+
+            // Request user input with ReadLine()
+            Console.WriteLine("Please enter an integer value an press enter");
+
+            // Assign the entered value to the var input
+            int input = Int32.Parse(Console.ReadLine());
+
+            // Check to see if the number is even
+            if(input % 2 == 0)
+                Console.WriteLine("The entered number was an even number.");
+            else 
+                Console.WriteLine("The entered number is not an even number.");
+
+        }
+    }
+}
