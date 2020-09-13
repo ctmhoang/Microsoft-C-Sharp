e061b5319c4222ec41d8ff2fe76d630dc77068a5
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2020-09-13 10:01:41 -0400

    Add the rest of module 2

---
+++ DEV204.1/ChessBoard/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace ChessBoard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Used a nested loop
+            // Used a decision structure to flip row output
+            // Output is correct per above image
+            bool done = false;
+            while (!done)
+            {
+                for (var i = 0; i < 8; i++)
+                {
+                    Console.WriteLine(i % 2 == 0
+                        ? string.Concat(Enumerable.Repeat("XO", 4))
+                        : string.Concat(Enumerable.Repeat("OX", 4)));
+                }
+
+                done = true;
+            }
+        }
+    }
+}
---
+++ DEV204.1/Mod2_Lab3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Mod2_Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine($"Counter is at: {counter}");
+            }
+
+            int outer;
+            int inner;
+
+            for (outer = 2; outer < 100; outer++)
+            {
+                for (inner = 2; inner <= (outer / inner); inner++)
+                {
+                    if(outer % inner == 0) break;
+                    if(inner > outer/inner) Console.WriteLine("{0} is prime", outer);
+                }
+            }
+        }
+    }
+}
