6b400ba3c3f60d3df34dc659c3f376d9361cc33e
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2020-09-24 05:55:42 -0400

    Finish haft of module 1

---
+++ DEV204.2/Mod1_Lab2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace Mod1_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating an object of Car() Class by using Object initializer
+            var car1 = new Car {Color = "White", Year = 2010, Mileage = 11000};
+
+            var car2 = new Car("Red", 2008);
+            int carCount = Car.CountCars();
+
+            Console.WriteLine($"There are {carCount} cars on inventory right now.");
+
+        }
+    }
+
+    // Declaring the Car() Class
+    // This class has 3 properties: Color, Year, and Mileage
+    public class Car
+    {
+        // Defining properties
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+
+        public Car(string color, int year) : this()
+        {
+            this.Color = color;
+            this.Year = year;
+        }
+
+        public Car(int year, int mileage) : this()
+        {
+            Year = year;
+            Mileage = mileage;
+        }
+
+        private static int _instances = 0;
+
+        public Car()
+        {
+            _instances++;
+        }
+
+        public static int CountCars()
+        {
+            return _instances;
+        }
+
+    }
+}
