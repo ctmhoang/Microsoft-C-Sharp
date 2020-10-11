8f5e17ead44b562f70f6f70e1f70173a625329de
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2020-10-11 06:01:45 -0400

    Add turtorial code

---
+++ DEV204.3/CarStruct/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarStruct
+{
+    struct Car
+    {
+        public string Make;
+        public string Model;
+        public string Color;
+
+        public void Accelerate()
+        {
+            Console.WriteLine("Vroom");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+            myCar.Make = "foo";
+            myCar.Model = "bar";
+            myCar.Color = "gold";
+            Console.WriteLine($"My car is a {myCar.Make} {myCar.Model} that is {myCar.Color} in color.");
+            myCar.Accelerate();
+        }
+    }
+}
