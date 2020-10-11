d9114d0fd2b10e187c7673869429a808db6ebec0
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2020-10-11 08:57:05 -0400

    Add mod 2 code tutorial

---
+++ DEV204.3/SC_Mod_Lab1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SC_Mod_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var myArrList =new ArrayList();
+            // myArrList.Add("First Item");
+            // myArrList.Add(3);
+            // myArrList.Add(4.5);
+            //
+            // foreach (object obj in myArrList)
+            // {
+            //     Console.WriteLine(obj.ToString());
+            // }
+
+            var myArrList = new ArrayList();
+            myArrList.Add("First Item");
+            myArrList.Add("Third Item");
+            myArrList.Add("Second Item");
+
+            myArrList.Sort();
+            int itemIndex = myArrList.IndexOf("Third Item");
+
+            foreach (object obj in myArrList)
+            {
+                Console.WriteLine(obj.ToString());
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Third Item is at index {itemIndex}.");
+
+        }
+    }
+}
---
+++ DEV204.3/SC_Mod_Lab2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SC_Mod_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var openWith = new Hashtable
+            {
+                {"txt", "notepad.exe"}, {"bmp", "paint.exe"}, {"dib", "paint.exe"}, {"rtf", "wordpad.exe"}
+            };
+
+            try
+            {
+                openWith.Add("txt", "winword.exe");
+            }
+            catch
+            {
+                Console.WriteLine("An element with Key = \"txt\" already exists.");
+            }
+            Console.WriteLine("For key = \"rtf\", value = {0}.", openWith["rtf"]);
+
+            openWith["rtf"] = "winword.exe";
+            Console.WriteLine("For key = \"rtf\", value = {0}.", openWith["rtf"]);
+
+            openWith["doc"] = "winword.exe"; // Add value without using add method
+
+            if (!openWith.ContainsKey("ht"))
+            {
+                openWith.Add("ht", "hypertrm.exe");
+                Console.WriteLine("Value added for key = \"ht\": {0}", openWith["ht"]);
+            }
+
+            Console.WriteLine();
+            foreach (DictionaryEntry de in openWith)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", de.Key, de.Value);
+            }
+
+        }
+    }
+}
