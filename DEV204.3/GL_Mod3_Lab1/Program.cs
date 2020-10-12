3aeae7a776bbcb02903caefcc5ee2e09b6429565
Cameron Hoang <51996720+ctmhoang@users.noreply.github.com>
2020-10-12 05:49:16 -0400

    Add the last tutorial code in this course

---
+++ DEV204.3/GL_Mod3_Lab1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GL_Mod3_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stud1 = new Student("Tom", "Thumb", 12, "Computer Science");
+            var stud3 = new Student("Mickey", "Mouse", 35, "Animation")
+                ;
+            var students = new List<Student>
+            {
+                stud1,
+                new Student("Fred", "Flintstone", 45, "Geology"),
+                stud3
+            };
+            foreach (Student stud in students)
+            {
+                Console.WriteLine(stud.FirstName);
+            }
+
+            bool exists = students.Contains<Student>(stud1);
+            Console.WriteLine(exists.ToString());
+            students.Remove(stud3);
+            Console.WriteLine(students.Count());
+        }
+    }
+
+    class Student
+    {
+        public Student(string first, string last, int age, string prog)
+        {
+            this.FirstName = first;
+            this.LastName = last;
+            this.Age = age;
+            this.Program = prog;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Program { get; set; }
+    }
+}
